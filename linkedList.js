918653826358556399beb34beeeb49020720a6c2
James Serengia <jamesserengia@gmail.com>
2024-05-01 21:25:30 +0300

    Add append and prepend methods

--- linkedList.js
+++ linkedList.js
@@ -12,8 +12,20 @@ class LinkedList {
     this.length = 0;
   }
 
+  printList() {
+    const arr = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      arr.push(currentNode.value);
+
+      currentNode = currentNode.next;
+    }
+    console.log(arr);
+  }
+
   append(value) {
     const newNode = new Node(value);
+    this.length++;
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
@@ -21,5 +33,29 @@ class LinkedList {
     }
 
     this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
+  }
+
+  prepend(value) {
+    this.length++;
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+    return this;
   }
 }
+
+const list = new LinkedList();
+list.append(10);
+list.append(7);
+list.append(13);
+list.printList();
+list.prepend(30);
+list.printList();
