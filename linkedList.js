7fe83743af66ef60fe3b328ba4376fcc97a9c00e
James Serengia <jamesserengia@gmail.com>
2024-05-01 21:12:24 +0300

    Start working on linkedList

---
+++ linkedList.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+
+    this.tail.next = newNode;
+  }
+}
