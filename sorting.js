aca2838f1c2b019a1f85e98d50077dc8525de820
James Serengia <jamesserengia@gmail.com>
2024-05-04 16:09:15 +0300

    Implement selection sort

--- sorting.js
+++ sorting.js
@@ -15,4 +15,27 @@ function bubbleSort(arr) {
 }
 
 const testArr = [16, 2, 67, 34, 5, 1, 0, 22];
-console.log(bubbleSort(testArr));
+// console.log(bubbleSort(testArr));
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let smallest = arr[i];
+    let smallestIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < smallest) {
+        smallest = arr[j];
+        smallestIndex = j;
+      }
+    }
+
+    if (arr[i] > smallest) {
+      // swap
+      const temp = arr[i];
+      arr[i] = smallest;
+      arr[smallestIndex] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort(testArr));
