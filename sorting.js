61c13520d4b7ae6aae5c4ad7ff616fd36ff85289
James Serengia <jamesserengia@gmail.com>
2024-05-06 19:07:49 +0300

    Merge sort implementation

--- sorting.js
+++ sorting.js
@@ -57,4 +57,40 @@ function insertionSort(arr) {
   return arr;
 }
 
-console.log(insertionSort(testArr));
+// console.log(insertionSort(testArr));
+
+function merge(arr1, arr2) {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) {
+      result.push(arr2[j]);
+      j++;
+    } else {
+      result.push(arr1[i]);
+      i++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr1[j]);
+    j++;
+  }
+}
+
+function mergeSort(arr) {
+  let mid = Math.floor(arr.length);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+console.log(mergeSort(testArr));
